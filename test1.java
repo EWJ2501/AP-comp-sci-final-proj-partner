922dc24fe525ef08a0f65415feb584db12fca987
MKN2501 <MKN2501@iolani.org>
2025-03-18 08:38:10 -1000

    2

--- test1.java
+++ test1.java
@@ -7,3 +7,19 @@ boolean startScreen = true;
 void setup () {
     size (2560, 1600);
 }
+
+void draw () {
+    if (startScreen){
+        background (100);
+        rectMode(CENTER);
+        strokeWeight(3);
+        rect (2560/2, 1600/2, 400, 150);
+        strokeWeight(1);
+        fill (255);
+    } else if (!startScreen && ! gameEnd){
+
+    } else if (gameEnd){
+
+    }
+
+}
